5bfe42f1ea2cfd86fd89e37f04934aa93b57d963
Shah Rokan <121371680+Rokan13@users.noreply.github.com>
2026-02-05 21:10:23 +0600

    condition

---
+++ conditional-statement.py
@@ -0,0 +1,14 @@
+age=12
+age2=18
+if(age>age2):
+    print("Age is greater than or equal to 18")
+else:
+    print("Age is less than or equal to 18")
+
+light=input("light colour: ")
+if(light=="red"):
+    print("stop")
+elif(light=="yellow"):
+    print("look")
+elif(light=="green"):
+    print("stop")
